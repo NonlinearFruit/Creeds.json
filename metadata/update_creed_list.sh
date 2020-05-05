95690aac553d6159f16e3cb12e73ce6d51ba98e8
NonlinearFruit <1123benji5813@gmail.com>
2020-05-05 17:41:20 -0500

    Add more metadata

--- metadata/update_creed_list.sh
+++ metadata/update_creed_list.sh
@@ -1,4 +1,4 @@
 vim -s metadata/clear_creeds.vim README.md
-for file in $(ls creeds/); do 
+for file in $(ls --reverse creeds/); do 
   jq '.Metadata | " - [x] [\(.Title) (\(.Year))](\(.SourceUrl))"' "creeds/${file}" --raw-output | xargs -0 -i sed -i "8i {}" README.md; 
 done
