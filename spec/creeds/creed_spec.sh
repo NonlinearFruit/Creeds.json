e52a0e6789aaea5c754eded267165ca4c817f776
NonlinearFruit <1123benji5813@gmail.com>
2021-08-28 16:55:28 -0500

    Test that all footnotes have proof texts (#23)

--- spec/creeds/creed_spec.sh
+++ spec/creeds/creed_spec.sh
@@ -7,15 +7,21 @@ Describe 'Creed'
   End
 
   is_ascii() { (file --exclude json $1 2> /dev/null || file json $1) | grep -q "ASCII"; }
-  all_proofs_in_use() { jq '[.. | objects | select(.Proofs != null) | . as $question | [keys[] | select(endswith("WithProofs")) | . as $key | $question[$key]] | join("") | contains("[\($question.Proofs[].Id)]")] | all' $1; }
+  all_proofs_have_footnote() { jq '[.. | objects | select(.Proofs != null) | . as $question | [keys[] | select(endswith("WithProofs")) | . as $key | $question[$key]] | join("") | contains("[\($question.Proofs[].Id)]")] | all' $1; }
+  all_footnotes_have_proofs() { jq '[.. | objects | . as $content | [(keys[] | select(endswith("WithProofs"))) as $key | $content[$key]] | select(.!=[]) | join("") | capture("\\[(?<id>[0-9]+)\\]";"g") | .id as $id | [$content.Proofs[].Id | tostring] | contains([$id])] | all' $1; }
 
   It "$1 is ascii only"
     When call is_ascii $1
     The status should not be failure
   End
 
-  It "$1 uses all the proof texts somewhere"
-    When call all_proofs_in_use $1
+  It "$1 has a footnote for each proof text"
+    When call all_proofs_have_footnote $1
+    The output should equal 'true'
+  End
+
+  It "$1 has a proof text for each footnote"
+    When call all_footnotes_have_proofs $1
     The output should equal 'true'
   End
 
