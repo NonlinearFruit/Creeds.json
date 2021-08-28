82e384f20c48656151e161c4a1532307f09aef98
NonlinearFruit <1123benji5813@gmail.com>
2021-08-27 23:51:09 -0500

    Test that all proof texts are used somewhere (#23)

--- spec/creeds/creed_spec.sh
+++ spec/creeds/creed_spec.sh
@@ -6,11 +6,17 @@ Describe 'Creed'
     done
   End
 
-  is_ascii() { file --exclude json $1 | grep -q "ASCII"; }
+  is_ascii() { (file --exclude json $1 2> /dev/null || file json $1) | grep -q "ASCII"; }
+  all_proofs_in_use() { jq '[.. | objects | select(.Proofs != null) | . as $question | [keys[] | select(endswith("WithProofs")) | . as $key | $question[$key]] | join("") | contains("[\($question.Proofs[].Id)]")] | all' $1; }
 
   It "$1 is ascii only"
     When call is_ascii $1
     The status should not be failure
+  End
+
+  It "$1 uses all the proof texts somewhere"
+    When call all_proofs_in_use $1
+    The output should equal 'true'
   End
 
   It "$1 matches the metadata schema"
