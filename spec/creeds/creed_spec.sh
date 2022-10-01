36c71d19121d9962c0ffae656ed346a8fdd97bf3
NonlinearFruit <NonlinearFruit@users.noreply.github.com>
2022-10-01 10:19:04 -0500

    All Nonproof Strings Have No Footnotes tests (#40)

--- spec/creeds/creed_spec.sh
+++ spec/creeds/creed_spec.sh
@@ -9,6 +9,7 @@ Describe 'Creed'
   is_ascii() { (file --exclude json $1 2> /dev/null || file json $1) | grep -q "ASCII"; }
   all_proofs_have_footnote() { jq '[.. | objects | select(.Proofs != null) | . as $question | [keys[] | select(endswith("WithProofs")) | . as $key | $question[$key]] | join("") | contains("[\($question.Proofs[].Id)]")] | all' $1; }
   all_footnotes_have_proofs() { jq '[.. | objects | . as $content | [(keys[] | select(endswith("WithProofs"))) as $key | $content[$key]] | select(.!=[]) | join("") | capture("\\[(?<id>[0-9]+)\\]";"g") | .id as $id | [$content.Proofs[].Id | tostring] | contains([$id])] | all' $1; }
+  all_nonproof_strings_have_no_footnotes() { jq '[.. | objects | . as $content | keys[] | select(endswith("WithProofs") | not) | $content[.] | type as $contentType | select($contentType == "string") | capture("\\[(?<id>[0-9]+)\\]";"g")] | length == 0' $1; }
 
   It "$1 is ascii only"
     When call is_ascii $1
@@ -28,6 +29,11 @@ Describe 'Creed'
   It "$1 matches the metadata schema"
     When call jsonschema -i $1 spec/creeds/Metadata.schema.json -F "ERROR: {error.path} {error.message}"
     The status should not be failure
+  End
+  
+  It "$1 has no footnotes in non-'WithProofs' strings"
+    When call all_nonproof_strings_have_no_footnotes $1
+    The output should equal 'true'
   End
 End
 
