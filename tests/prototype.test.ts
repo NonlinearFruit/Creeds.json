7a67a79a9233e066bb4a6433d57f82889cf35142
NonlinearFruit <1123benji5813@gmail.com>
2023-03-13 17:49:55 -0500

    Implement some creed tests

--- tests/prototype.test.ts
+++ tests/prototype.test.ts
@@ -11,12 +11,35 @@ interface Metadata {
   eAlternativeTitles: string[]
 }
 
-describe('suite', () => {
-  test('serial test', async () => { 
-    let data = require('../creeds/zwinglis_67_articles.json') 
-    expect(data).toBeTruthy()
-    data as Resource
-    expect(data.Metadata).toBeTruthy()
-    expect(data.Metadata.Title).toBe("Zwingli's 67 Articles")
+let fs = require('fs');
+let path = require('path');
+let repoPath = require('path').resolve(__dirname, '..')
+let creedFolder = repoPath+'/creeds'
+let files = fs.readdirSync(creedFolder)
+let Validator = require('jsonschema').Validator;
+let validator = new Validator()
+let metadataSchema = require(__dirname+'/Metadata.schema.json')
+
+describe.each(files)('%s', (filename) => {
+  let creed = require(creedFolder+'/'+filename)
+
+  test('has metadata', async () => {
+    let result = validator.validate(creed, metadataSchema)
+    expect(result.valid, result).toBeTruthy()
+  })
+
+  test('matches schema', async () => {
+    let schemaFormat = creed.Metadata.CreedFormat;
+    let schema = require(__dirname+'/'+schemaFormat+'.schema.json')
+    let result = validator.validate(creed, schema)
+    expect(result.valid, result).toBeTruthy()
+  })
+
+  test('is ascii', async () => {
+    function isAscii(str) {
+          return /^[\x00-\x7F]*$/.test(str);
+    }
+    let fileContent = fs.readFileSync(creedFolder+'/'+filename)
+    expect(isAscii(creed)).toBeTruthy()
   })
 })
