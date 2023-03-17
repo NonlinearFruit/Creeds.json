5ce75f420ed12232fa58a47f3cf7fac2e2a09236
NonlinearFruit <1123benji5813@gmail.com>
2023-03-16 23:10:29 -0500

    Finish rewriting the creed tests into typescript

--- tests/prototype.test.ts
+++ tests/prototype.test.ts
@@ -30,4 +30,63 @@ describe.each(files)('%s', (filename) => {
     let fileContent = fs.readFileSync(`${creedFolder}/${filename}`)
     expect(isAscii(creed)).toBeTruthy()
   })
+
+  test('footnote for each proof text', async () => {
+    let data = creed.Data
+    if (!(data instanceof Array))
+      data = [data]
+    for (const item of data)
+    {
+      if (!item.Proofs || !(item.Proofs instanceof Array))
+        continue
+      const text = Object
+        .entries(item)
+        .filter(([key, _]) => key.endsWith('WithProofs'))
+        .map(([_, value]) => value)
+        .reduce((a, b) => a + b, '')
+      for (const proof of item.Proofs)
+        expect(text).toContain(`[${proof.Id}]`)
+    }
+  })
+
+  test('proof text for each footnote', async () => {
+    let data = creed.Data
+    if (!(data instanceof Array))
+      data = [data]
+    for (const item of data)
+    {
+      if (!item.Proofs || !(item.Proofs instanceof Array))
+        continue
+      const footnoteIds = Object
+        .entries(item)
+        .filter(([key, _]) => key.endsWith('WithProofs'))
+        .map(([_, textWithProofs]) => textWithProofs)
+        .reduce((a, b) => a + b, '')
+        .match(/(?<=\[)\d+(?=\])/g)
+      const proofTextIds = item
+        .Proofs
+        .map((proof) => proof.Id.toString())
+      if (footnoteIds)
+        for (const footnoteId of footnoteIds)
+          expect(proofTextIds).toContain(footnoteId)
+    }
+  })
+
+  test('no footnotes in non-WithProofs strings', async () => {
+    let data = creed.Data
+    if (!(data instanceof Array))
+      data = [data]
+    for (const item of data)
+    {
+      if (!item.Proofs || !(item.Proofs instanceof Array))
+        continue
+      const footnoteIds = Object
+        .entries(item)
+        .filter(([key, _]) => !key.endsWith('WithProofs'))
+        .map(([_, textWithoutProofs]) => textWithoutProofs)
+        .reduce((a, b) => a + b, '')
+        .match(/(?<=\[)\d+(?=\])/g)
+      expect(footnoteIds).toBeNull()
+    }
+  })
 })
