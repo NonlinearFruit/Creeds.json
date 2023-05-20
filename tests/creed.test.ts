a85a631b9f10331af1136e43479c35645946f24c
NonlinearFruit <1123benji5813@gmail.com>
2023-05-19 21:03:35 -0500

    Organize ts tests

---
+++ tests/creed.test.ts
@@ -0,0 +1,96 @@
+import { resolve } from "path";
+import { readdirSync, readFileSync } from "fs"
+import { describe, test, expect } from "vitest"
+import { Validator } from "jsonschema"
+import TJS from "typescript-json-schema"
+
+const validator = new Validator()
+
+const compilerOptions: TJS.CompilerOptions = {
+  strictNullChecks: true,
+}
+const program = TJS.getProgramFromFiles(
+  [resolve("tests/types.ts")],
+  compilerOptions
+)
+const settings: TJS.PartialArgs = {
+  required: true,
+}
+
+const repoPath = resolve(__dirname, '..')
+const creedFolder = `${repoPath}/creeds`
+const files = readdirSync(creedFolder)
+const testData = files
+  .map(filename => ({
+    filename,
+    creed: require(`${creedFolder}/${filename}`)
+  }))
+  .filter(testData => testData.creed.Metadata.CreedFormat == "Creed")
+
+describe.each(testData)('$filename', ({filename, creed}) => {
+
+  test('has metadata', async () => {
+    const schema = TJS.generateSchema(program, "Metadata", settings)
+
+    const result = validator.validate(creed.Metadata, schema)
+
+    expect(result.valid, result).toBeTruthy()
+  })
+
+  test(`matches ${creed.Metadata.CreedFormat} schema`, async () => {
+    const schema = TJS.generateSchema(program, creed.Metadata.CreedFormat, settings)
+
+    const result = validator.validate(creed, schema)
+
+    expect(result.valid, result).toBeTruthy()
+  })
+
+  test('is ascii', async () => {
+    let buf = readFileSync(`${creedFolder}/${filename}`)
+    const len=buf.length
+    for (let i=0; i<len; i++)
+      expect(buf[i]).toBeLessThan(127)
+  })
+
+  const item = creed.Data
+
+  test('each footnote has a proof text', () => {
+    if (!item.Proofs || !(item.Proofs instanceof Array))
+      return
+    const footnoteIds = Object
+      .entries(item)
+      .filter(([key, _]) => key.endsWith('WithProofs'))
+      .map(([_, textWithProofs]) => textWithProofs)
+      .reduce((a, b) => a + b, '')
+      .match(/(?<=\[)\d+(?=\])/g)
+    const proofTextIds = item
+      .Proofs
+      .map((proof) => proof.Id.toString())
+    if (footnoteIds)
+      for (const footnoteId of footnoteIds)
+        expect(proofTextIds).toContain(footnoteId)
+  })
+
+  test('each proof text has a footnote', async () => {
+    if (!item.Proofs || !(item.Proofs instanceof Array))
+      return
+    const text = Object
+      .entries(item)
+      .filter(([key, _]) => key.endsWith('WithProofs'))
+      .map(([_, value]) => value)
+      .reduce((a, b) => a + b, '')
+    for (const proof of item.Proofs)
+      expect(text).toContain(`[${proof.Id}]`)
+  })
+
+  test('no footnotes in non-WithProofs strings', async () => {
+    const footnoteIds = Object
+      .entries(item)
+      .filter(([key, _]) => !key.endsWith('WithProofs'))
+      .filter(([_, obj]) => typeof obj == "string")
+      .map(([_, textWithoutProofs]) => textWithoutProofs)
+      .reduce((a, b) => a + b, '')
+      .match(/(?<=\[)\d+(?=\])/g)
+    expect(footnoteIds).toBeNull()
+  })
+})
