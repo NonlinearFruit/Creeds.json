a2c6e14802e8d40f6048b108b187622192880b3b
James Chen-Smith <james@chen-smith.net>
2023-08-18 11:56:51 -0500

    Extract proof tests into common tests

--- tests/common.ts
+++ tests/common.ts
@@ -57,12 +57,56 @@ export const testDocument = (document: CreedDocument<any>, filename: string) => 
   })
 }
 
-export const testReferences = (proofs: Proof[]) => {
+const testReferences = (proofs: Proof[]) => {
   test('each proof reference is valid', async () => {
     if (!proofs || !(proofs instanceof Array))
       return
     for (const proof of proofs) {
       expect(proof.References).toBeInstanceOf(Array)
     }
+  })
+}
+
+export const testProofs = (item: any) => {
+  testReferences(item.Proofs)
+
+  test('each footnote has a proof text', () => {
+    if (!item.Proofs || !(item.Proofs instanceof Array))
+      return
+    const footnoteIds = Object
+      .entries(item)
+      .filter(([key, _]) => key.endsWith('WithProofs'))
+      .map(([_, textWithProofs]) => textWithProofs)
+      .reduce((a, b) => a + b, '')
+      .match(/(?<=\[)\d+(?=\])/g)
+    const proofTextIds = item
+      .Proofs
+      .map((proof) => proof.Id.toString())
+    if (footnoteIds)
+      for (const footnoteId of footnoteIds)
+        expect(proofTextIds).toContain(footnoteId)
+  })
+
+  test('each proof text has a footnote', async () => {
+    if (!item.Proofs || !(item.Proofs instanceof Array))
+      return
+    const text = Object
+      .entries(item)
+      .filter(([key, _]) => key.endsWith('WithProofs'))
+      .map(([_, value]) => value)
+      .reduce((a, b) => a + b, '')
+    for (const proof of item.Proofs)
+      expect(text).toContain(`[${proof.Id}]`)
+  })
+
+  test('no footnotes in non-WithProofs strings', async () => {
+    const footnoteIds = Object
+      .entries(item)
+      .filter(([key, _]) => !key.endsWith('WithProofs'))
+      .filter(([_, obj]) => typeof obj == "string")
+      .map(([_, textWithoutProofs]) => textWithoutProofs)
+      .reduce((a, b) => a + b, '')
+      .match(/(?<=\[)\d+(?=\])/g)
+    expect(footnoteIds).toBeNull()
   })
 }
--- tests/henryscatechism.test.ts
+++ tests/henryscatechism.test.ts
@@ -1,9 +1,9 @@
-import { describe, test, expect } from "vitest"
-import { testData, testReferences, testDocument } from "./common.ts"
+import { describe } from "vitest"
+import { testData, testDocument, testProofs } from "./common.ts"
 
 const type = "HenrysCatechism"
 
-describe.each(testData[type])('$filename', ({filename, filepath, creed}) => {
+describe.each(testData[type])('$filename', ({filepath, creed}) => {
 
   testDocument(creed, filepath)
 
@@ -18,48 +18,7 @@ describe.each(testData[type])('$filename', ({filename, filepath, creed}) => {
       title: "Content",
       item: data
     }]
-  describe.each(data)('$title', ({title, item}) => {
-
-    testReferences(item.Proofs)
-
-    test('each footnote has a proof text', () => {
-      if (!item.Proofs || !(item.Proofs instanceof Array))
-        return
-      const footnoteIds = Object
-        .entries(item)
-        .filter(([key, _]) => key.endsWith('WithProofs'))
-        .map(([_, textWithProofs]) => textWithProofs)
-        .reduce((a, b) => a + b, '')
-        .match(/(?<=\[)\d+(?=\])/g)
-      const proofTextIds = item
-        .Proofs
-        .map((proof) => proof.Id.toString())
-      if (footnoteIds)
-        for (const footnoteId of footnoteIds)
-          expect(proofTextIds).toContain(footnoteId)
-    })
-
-    test('each proof text has a footnote', async () => {
-      if (!item.Proofs || !(item.Proofs instanceof Array))
-        return
-      const text = Object
-        .entries(item)
-        .filter(([key, _]) => key.endsWith('WithProofs'))
-        .map(([_, value]) => value)
-        .reduce((a, b) => a + b, '')
-      for (const proof of item.Proofs)
-        expect(text).toContain(`[${proof.Id}]`)
-    })
-
-    test('no footnotes in non-WithProofs strings', async () => {
-      const footnoteIds = Object
-        .entries(item)
-        .filter(([key, _]) => !key.endsWith('WithProofs'))
-        .filter(([_, obj]) => typeof obj == "string")
-        .map(([_, textWithoutProofs]) => textWithoutProofs)
-        .reduce((a, b) => a + b, '')
-        .match(/(?<=\[)\d+(?=\])/g)
-      expect(footnoteIds).toBeNull()
-    })
+  describe.each(data)('$title', ({item}) => {
+    testProofs(item)
   })
 })
